de65173e40a975e6fdbe0ce2d8133d28f27533be
Shreyas Chaudhari <shreya.chaudhari@sbdinc.com>
2021-03-13 12:03:47 +0530

    Added advaned redux

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -32,7 +32,7 @@ export const initIngredients = () => {
   return (dispatch) => {
     axios
       .get(
-        'https://react-my-burger-e5968-default-rtdb.firebaseio.com/ingredients. '
+        'https://react-my-burger-e5968-default-rtdb.firebaseio.com/ingredients.json'
       )
       .then((response) => {
         dispatch(setIngredients(response.data));
