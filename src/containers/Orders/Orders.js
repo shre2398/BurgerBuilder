61cb439fa17948c01f651c129cd7f218b368f6ea
Shreyas Chaudhari <shreya.chaudhari@sbdinc.com>
2021-03-16 18:44:52 +0530

    Updating UI depending on auth status

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -8,39 +8,40 @@ import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
-    componentDidMount () {
-        this.props.onFetchOrders();
-    }
+  componentDidMount() {
+    this.props.onFetchOrders(this.props.token);
+  }
 
-    render () {
-        let orders = <Spinner />;
-        if ( !this.props.loading ) {
-            orders = this.props.orders.map( order => (
-                <Order
-                    key={order.id}
-                    ingredients={order.ingredients}
-                    price={order.price} />
-            ) )
-        }
-        return (
-            <div>
-                {orders}
-            </div>
-        );
+  render() {
+    let orders = <Spinner />;
+    if (!this.props.loading) {
+      orders = this.props.orders.map((order) => (
+        <Order
+          key={order.id}
+          ingredients={order.ingredients}
+          price={order.price}
+        />
+      ));
     }
+    return <div>{orders}</div>;
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        orders: state.order.orders,
-        loading: state.order.loading
-    };
+const mapStateToProps = (state) => {
+  return {
+    orders: state.order.orders,
+    loading: state.order.loading,
+    token: state.auth.token
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onFetchOrders: () => dispatch( actions.fetchOrders() )
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
+  };
 };
 
-export default connect( mapStateToProps, mapDispatchToProps )( withErrorHandler( Orders, axios ) );
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withErrorHandler(Orders, axios));
